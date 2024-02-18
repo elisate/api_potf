30914db7dbcf243f0f20c85399e295fd076c9c70
elisate <dushimiyimanaelisa@gmail.com>
2024-02-18 21:42:01 +0200

    initial project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app=express()
+//routes
+app.get('/',(req,res)=>
+{
+    res.send('hello node api')
+}
+)
+app.listen(3000, ()=>{
+    console.log('node api is running on port 3000')
+}
+)
